29f5d3df10040aae6275290405b94526884626f8
NyunuBit <nyunu@proton.me>
2026-05-23 22:05:19 -0300

    feat: initialized the states

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod plugins;
 mod prelude;
 
-use plugins::window;
+use plugins::{game_state, window};
 use prelude::*;
 
 fn main() {
-    App::new().add_plugins(window::WindowHandlingPlugin).run();
+    App::new()
+        .add_plugins((window::WindowHandlingPlugin, game_state::GameStatePlugin))
+        .run();
 }
--- src/plugins/game_state.rs
+++ src/plugins/game_state.rs
@@ -1,8 +1,25 @@
 use bevy::prelude::*;
 
 #[derive(States, Debug, Clone, Eq, PartialEq, Hash, Default)]
-enum GameState {
+pub enum AppState {
     #[default]
+    Loading,
     Menu,
     InGame,
+}
+
+#[derive(States, Debug, Clone, Eq, PartialEq, Hash, Default)]
+pub enum PausedState {
+    #[default]
+    Paused,
+    Resumed,
+}
+
+pub struct GameStatePlugin;
+
+impl Plugin for GameStatePlugin {
+    fn build(&self, app: &mut App) {
+        app.init_state::<AppState>();
+        app.init_state::<PausedState>();
+    }
 }
